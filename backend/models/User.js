931f8baa794665b9e81c0cd3e8efb4e15c2c5473
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-14 16:01:52 +0600

    feat: Create Userschema and User model

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+      type: String,
+      default: null,
+    },
+    otp: {
+      type: String,
+    },
+    isOtpVerified: {
+      type: Boolean,
+      default: false,
+    },
+    otpExpires: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
