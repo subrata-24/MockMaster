28d7c98cb5dbdef35198282527d4931aafae1233
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-15 02:03:47 +0600

    feat: Create database and connect it with backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -20,5 +22,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running at http://localhost:${PORT}`);
 });
