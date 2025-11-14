f637088dd0b6c79306b0602fbb1a5b8caa633f7c
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-14 16:11:45 +0600

    feat: Create question schema and question model for question and answer

---
+++ backend/models/Question.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema(
+  {
+    session: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "session",
+    },
+    question: String,
+    answer: String,
+    note: String,
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Question = mongoose.model("Question", questionSchema);
+
+export default Question;
