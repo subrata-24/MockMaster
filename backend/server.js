2621798ca38daba4f6e0df63744d69fc96995844
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-14 13:44:15 +0600

    feat: Make the express server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//built-in middleware to understand and parse json JSON data sent from the client
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello vaiya");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
