2a7ffe2bbd8a3fdbfbb713e48deef6182ffee5fb
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-12-02 03:15:13 +0600

    feat(ai): implement question explanation generation using Gemini
    - Added questionExplanation controller to generate concept explanations
    - Validates input to ensure a question is provided before processing
    - Uses conceptExplainPrompt to build structured explanation prompt
    - Calls Gemini model `gemini-2.5-flash` for explanation generation
    - Cleans AI output by removing markdown JSON code fences
    - Parses clean JSON into object and returns structured response
    - Includes error handling for LLM output issues and JSON parsing errors

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-import { questionAnswerPrompt } from "../utils/prompt.js";
+import { conceptExplainPrompt, questionAnswerPrompt } from "../utils/prompt.js";
 
 // Initialize Google Gemini AI client with API key from environment variables
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
@@ -43,6 +43,43 @@ export const generateInterviewsQuestion = async (req, res) => {
     return res.status(200).json({ success: true, data });
   } catch (error) {
     console.log("Find error while creating question and answer", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server error", error });
+  }
+};
+
+export const questionExplanation = async (req, res) => {
+  try {
+    const { question } = req.body;
+    if (!question) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Must provide a question" });
+    }
+
+    const prompt = conceptExplainPrompt(question);
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+
+    const rawText = response.text;
+
+    const cleanText = rawText
+      .replace(/^```json\s*/, "") //remove starting ```json
+      .replace(/```$/, "") // remove ending ```
+      .trim(); // remove extra space
+
+    const data = JSON.parse(cleanText);
+    return res.status(200).json({
+      success: true,
+      message: "Expalantion generate successfully",
+      data,
+    });
+  } catch (error) {
+    console.log("Find error when creating explanation of answer", error);
     return res
       .status(500)
       .json({ success: false, message: "Server error", error });
--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,6 +1,10 @@
 import express from "express";
 import isAuth from "../middlewares/authMiddleware.js";
-import { generateInterviewsQuestion } from "../controllers/aiController.js";
+import {
+  generateInterviewsQuestion,
+  questionExplanation,
+} from "../controllers/aiController.js";
 
 export const aiRouter = express.Router();
 aiRouter.post("/question-answer", isAuth, generateInterviewsQuestion);
+aiRouter.post("/question-explanation", questionExplanation);
